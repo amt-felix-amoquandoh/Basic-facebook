8ac10296c9f9cd724e521905dc3848d00ddf6af8
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-07-07 00:19:39 +0000

    first commit

---
+++ js/sript.js
@@ -0,0 +1,39 @@
+var dataBase = [
+    {
+        username : 'asher',
+        password : 'nothing'
+    },
+
+    {
+        username : 'mimi',
+        password : 'something'
+    },
+
+]
+
+var timeLine = [
+    {
+        username : 'nissi',
+        password : 'im a diamond'
+    },
+    {
+        username : 'david',
+        password : 'call me Nyame Ay3, i feel good'
+    },
+    {
+        username : 'zion',
+        password : 'Star boy is my name'
+    },
+    {
+        username : 'bumike',
+        password : 'Im one in a million'
+    },
+    {
+        username : 'asher',
+        password : 'my delicacies are fit for a king'
+    },
+]
+
+
+var enterUsername = prompt('Who are you?');
+var enterPassword = prompt('enter your password');
