cbd73c90eee435566ecf2eb01a247b1a970ad07f
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-07-08 15:13:15 +0000

    update sript.js

--- js/sript.js
+++ js/sript.js
@@ -4,6 +4,11 @@ var dataBase = [
         password : 'nothing'
     },
 
+    {
+        username : 'mimi',
+        password : 'something'
+    },
+
 ]
 
 var timeLine = [
@@ -30,15 +35,27 @@ var timeLine = [
 ]
 
 
+
+
+function validUser (username, password) {
+    for ( i = 0; i < dataBase.length; i++){
+        if(dataBase[i].username === username && dataBase[i].password === password){
+            return true;
+        } 
+    }
+    return false;
+}
+
+function signIn(username, password){
+    if(validUser(username,password)){
+        console.log(timeLine);
+    } else{
+        alert('sorry wrong user');
+    }
+    
+}
+
 var enterUsername = prompt('Who are you?');
 var enterPassword = prompt('enter your password');
-
-function signIn (username, password){
-   if( username === dataBase[0].username && password === dataBase[0].password){
-         console.log('whew!!!!!!');
-   } else {
-      alert('sorry wrong user info');
-   }
-} 
 
 signIn(enterUsername, enterPassword);
